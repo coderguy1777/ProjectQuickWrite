e94ed34d93197f2940a25509964826802e5a0141
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-10-19 12:45:40 -0400

    Add files via upload

--- src/MainTextreadingClass.java
+++ src/MainTextreadingClass.java
@@ -3,26 +3,45 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class MainTextreadingClass {
+    private static ArrayList<String> sentences = new ArrayList<String>();
+    private static ArrayList<Float> Wordvectors = new ArrayList<Float>();
 
+    private static double[][]matricies = new double[10000][10000];
+    private static double[][]wordvectorarray = new double[2000][2000];
     private static char textstorevalue;
-    private static String sentencestorage;
+    public static String sentencestorage;
     private static double textpoint;
     private static double sentencelength;
-    public static void main(String[]args) {
+
+
+    public static Scanner fileclass() {
         NeuronClass neuralfunction;
         neuralfunction = new NeuronClass();
         try {
             Scanner datareader = new Scanner(new BufferedReader(new FileReader("Essay1.txt")));
             while(datareader.hasNext()) {
-                String[][]SentenceArray = new String[1000][1000];
                 sentencelength = 100;
-                sentencestorage = datareader.nextLine();
+                sentencestorage = datareader.next();
+                sentences.add(sentencestorage);
             }
 
         } catch(IOException e) {
             e.printStackTrace();
             System.out.print(e);
         }
+        return null;
+    }
+
+    public static void wordvectormaker(ArrayList<Float>Wordvectors) {
 
     }
+
+    public static ArrayList<ArrayList<Double>>wordvectorsorter() {
+        return null;
+    }
+
+    private static ArrayList<Double> vectormatricies(ArrayList<Double>Wordvector) {
+        return Wordvector;
+    }
+
 }
